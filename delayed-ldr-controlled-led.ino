05cfe9e01f7558c948b93a453df5333243a6fd0f
Stephen Oberholtzer <stevie@qrpff.net>
2017-03-10 20:54:03 -0500

    Read A0 and A5 and output them when the button is pressed
    On my board, apparently:
    
    A0 = pot (0 = towards Nano side; 1023 = towards the side with the 2.5mm jacks)

--- delayed-ldr-controlled-led.ino
+++ delayed-ldr-controlled-led.ino
@@ -9,6 +9,8 @@ void setup() {
   Serial.print("sizeof(void*) = "); Serial.print(sizeof(void*)); Serial.println("");
 
   pinMode(PD3, INPUT);
+  pinMode(A5, INPUT);
+  pinMode(A0, INPUT);
 }
 
 uint8_t prev_pd3 = LOW;
@@ -17,7 +19,9 @@ void loop() {
   // put your main code here, to run repeatedly:
   uint8_t pd3 = digitalRead(PD3);
   if (pd3 && !prev_pd3) {
-    Serial.println("button activated");
+    //Serial.println("button activated");
+    Serial.print("A5 = "); Serial.println(analogRead(A5));    
+    Serial.print("A0 = "); Serial.println(analogRead(A0));
   }
   prev_pd3 = pd3;
 }
