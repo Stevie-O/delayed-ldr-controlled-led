8b8e25444688265644a4baf750061fec5853db47
Stephen Oberholtzer <stevie@qrpff.net>
2017-03-10 20:33:46 -0500

    Now I know how big ints and pointers are

--- delayed-ldr-controlled-led.ino
+++ delayed-ldr-controlled-led.ino
@@ -5,10 +5,14 @@ void setup() {
   Serial.begin(115200);
 
   Serial.println("Hello, world!");
+  Serial.print("sizeof(int) = "); Serial.print(sizeof(int)); Serial.println("");
+  Serial.print("sizeof(void*) = "); Serial.print(sizeof(void*)); Serial.println("");
+
+  pinMode(PD3, INPUT);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  delay(100);
 }
